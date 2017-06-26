7f521934035c011af008c261bd8af604818a249a
Chris Crebolder <ccrebolder@gmail.com>
2017-06-26 19:13:56 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_handler_test
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef_handler_test
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
